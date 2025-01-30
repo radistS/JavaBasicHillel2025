059cb2871a994757bf9a11902c9ed1acf66ca05d
oleksandr.stepurko <stepurko.alexandr@gmail.com>
2025-01-30 19:43:29 +0100

    first commit

---
+++ javaBasic/src/lec02/Comments.java
@@ -0,0 +1,22 @@
+package lec02;
+
+public class Comments {
+    /**
+     * @since
+     * @deprecated
+     */
+    public static void main(String[] args) {
+
+        // single line comment
+
+
+
+        /*  lines
+          1 - line
+          2 - line
+          ...
+          n- line
+         */
+
+    }
+}
---
+++ javaBasic/src/lec02/view/pack1/P1C2.java
@@ -0,0 +1,13 @@
+package lec02.view.pack1;
+
+public class P1C2 {
+
+    public static void main(String[] args) {
+        P1C1 cl1 = new P1C1();
+
+        cl1.printPublic();
+
+        cl1.printProtected();
+
+    }
+}
