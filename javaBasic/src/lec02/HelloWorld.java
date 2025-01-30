5f9ab35c4d8a9594ecf38668e8870d5233d09116
oleksandr.stepurko <stepurko.alexandr@gmail.com>
2025-01-30 19:43:29 +0100

    Lesson_3

---
+++ javaBasic/src/lec02/HelloWorld.java
@@ -0,0 +1,9 @@
+package lec02;
+
+public class HelloWorld {
+
+    public static void main(String[] args) { // psvm
+        System.out.println("Hello word"); // sout
+     }
+
+}
---
+++ javaBasic/src/lec02/view/pack2/P2C2.java
@@ -0,0 +1,12 @@
+package lec02.view.pack2;
+
+import lec02.view.pack1.P1C1;
+
+public class P2C2 extends P1C1 {
+    @Override
+    protected void printProtected() {
+        System.out.println("Protected - c2p2");
+        super.printProtected();
+
+    }
+}
