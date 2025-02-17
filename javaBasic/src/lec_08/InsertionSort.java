fabc3b7a2fc2b5437add192cc0d95df3c974884d
oleksandr.stepurko <stepurko.alexandr@gmail.com>
2025-02-17 20:08:19 +0100

    Lesson_8

---
+++ javaBasic/src/lec_08/InsertionSort.java
@@ -0,0 +1,7 @@
+package lec_08;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        System.out.println("https://www.geeksforgeeks.org/insertion-sort-algorithm/");
+    }
+}
---
+++ javaBasic/src/lec_08/task/Task3.java
@@ -0,0 +1,9 @@
+package lec_08.task;
+
+public class Task3 {
+    public static void main(String[] args) {
+        String str = "banana";
+        str.replace("a", "o");
+        System.out.println(str);
+    }
+}
